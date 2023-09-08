0a43c5bc3af786fcd0eb0e430b5c47627662f8df
henawah <henawah90@gmail.com>
2023-09-08 22:56:30 +0700

    first commit

---
+++ code.js
@@ -0,0 +1,5 @@
+function curriedTambah(a) {
+    return function(b) {
+        return a + b;
+    };
+}
---
+++ switch.js
@@ -0,0 +1,12 @@
+var hari = "Senin";
+
+switch (hari) {
+  case "Senin":
+    console.log("Hari kerja.");
+    break;
+  case "Minggu":
+    console.log("Hari libur.");
+    break;
+  default:
+    console.log("Hari tidak valid.");
+}
